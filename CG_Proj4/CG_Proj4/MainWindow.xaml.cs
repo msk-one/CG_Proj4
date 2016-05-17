56eeab7ed3585f1d52857d58dd40eeed3bd6d515
Msk <msk.sk.one@gmail.com>
2016-05-17 10:10:43 +0200

    GUI ready

--- CG_Proj4/CG_Proj4/MainWindow.xaml.cs
+++ CG_Proj4/CG_Proj4/MainWindow.xaml.cs
@@ -15,14 +15,38 @@ using System.Windows.Shapes;
 
 namespace CG_Proj4
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private bool mouseClicked = true;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void mainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseClicked)
+            {
+                coord_X.Content = "X: " + e.GetPosition(mainCanvas).X;
+                coord_Y.Content = "Y: " + e.GetPosition(mainCanvas).Y;
+            }
+        }
+
+        private void mainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (mouseClicked) { mouseClicked = false; }
+            else { mouseClicked = true; }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string coordsX = coord_X.Content as string;
+            string coordsY = coord_Y.Content as string;
+
+            X1.Text = coordsX.Substring(3);
+            Y1.Text = coordsY.Substring(3);
+
+            mouseClicked = true;
         }
     }
 }
