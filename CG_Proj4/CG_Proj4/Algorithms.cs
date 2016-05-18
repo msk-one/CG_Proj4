2afacc4e31d426b8ff827f0f8de5fbb69f907ef2
Msk <msk.sk.one@gmail.com>
2016-05-18 19:23:25 +0200

    Final

--- CG_Proj4/CG_Proj4/Algorithms.cs
+++ CG_Proj4/CG_Proj4/Algorithms.cs
@@ -9,6 +9,117 @@ namespace CG_Proj4
 {
     public class Algorithms
     {
+        public static void FloodFill4X(WriteableBitmap bmp, Point pt, Color target, Color repl)
+        {
+            Stack<Point> pxStack = new Stack<Point>();
+            target = bmp.GetPixel((int)pt.X, (int)pt.Y);
 
+            pxStack.Push(pt);
+
+            while (pxStack.Count > 0)
+            {
+                Point a = pxStack.Pop();
+
+                if (a.X < bmp.Width && a.X > 0 && a.Y > 0 && a.Y < bmp.Height)
+                {
+                    if (bmp.GetPixel((int)a.X, (int)a.Y) == target)
+                    {
+                        bmp.SetPixel((int)a.X, (int)a.Y, repl);
+                        pxStack.Push(new Point(a.X - 1, a.Y));
+                        pxStack.Push(new Point(a.X + 1, a.Y));
+                        pxStack.Push(new Point(a.X, a.Y - 1));
+                        pxStack.Push(new Point(a.X, a.Y + 1));
+                    }
+                }
+            }
+            
+        }
+
+        public static void FloodFill8X(WriteableBitmap bmp, Point pt, Color target, Color repl)
+        {
+            Stack<Point> pxStack = new Stack<Point>();
+            target = bmp.GetPixel((int)pt.X, (int)pt.Y);
+
+            pxStack.Push(pt);
+
+            while (pxStack.Count > 0)
+            {
+                Point a = pxStack.Pop();
+
+                if (a.X < bmp.Width && a.X > 0 && a.Y > 0 && a.Y < bmp.Height)
+                {
+                    if (bmp.GetPixel((int)a.X, (int)a.Y) == target)
+                    {
+                        bmp.SetPixel((int)a.X, (int)a.Y, repl);
+
+                        pxStack.Push(new Point(a.X - 1, a.Y + 1));
+                        pxStack.Push(new Point(a.X - 1, a.Y));
+                        pxStack.Push(new Point(a.X + 1, a.Y));
+                        pxStack.Push(new Point(a.X, a.Y - 1));
+                        pxStack.Push(new Point(a.X - 1, a.Y - 1));
+                        pxStack.Push(new Point(a.X + 1, a.Y + 1));
+                        pxStack.Push(new Point(a.X, a.Y + 1));
+                        pxStack.Push(new Point(a.X + 1, a.Y - 1));
+                    }
+                }
+            }
+
+        }
+
+        public static void Scanline(WriteableBitmap bmp, Point pt, Color target, Color repl)
+        {
+            Stack<Point> pxStack = new Stack<Point>();
+
+            target = bmp.GetPixel((int)pt.X, (int)pt.Y);
+
+            int ymax;
+            bool left;
+            bool right;
+
+            pxStack.Push(pt);
+
+            while (pxStack.Count != 0) {
+                Point a = pxStack.Pop();
+                ymax = (int) a.Y;
+
+                while (ymax > 0 && bmp.GetPixel((int)a.X, ymax) == target)
+                {
+                    ymax--;
+                }
+
+                ymax++;
+
+                left = false;
+                right = false;
+
+                while (ymax < bmp.Height && bmp.GetPixel((int)a.X, ymax) == target)
+                {
+                    bmp.SetPixel((int)a.X, ymax, repl);
+
+                    if (!right && a.X < bmp.Width - 1 && bmp.GetPixel((int)a.X + 1, ymax) == target)
+                    {
+                        pxStack.Push(new Point(a.X + 1, ymax));
+                        right = true;
+                    }
+                    else if (right && a.X < bmp.Width - 1 && bmp.GetPixel((int)a.X + 1, ymax) != target)
+                    {
+                        right = false;
+                    }
+                    if (!left && a.X > 0 && bmp.GetPixel((int)a.X - 1, ymax) == target)
+                    {
+                        pxStack.Push(new Point(a.X - 1, ymax));
+                        left = true;
+                    }
+                    else if (left && a.X - 1 == 0 && bmp.GetPixel((int)a.X - 1, ymax) != target)
+                    {
+                        left = false;
+                    }
+                    
+                    ymax++;
+                }
+
+            }
+
+        }
     }
 }
--- CG_Proj4/CG_Proj4/MainWindow.xaml.cs
+++ CG_Proj4/CG_Proj4/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace CG_Proj4
         public List<Point> polyPoints { get; set; }
         private bool mouseClicked = true;
         private Random rnd;
+
+        public WriteableBitmap writeableBmp;
         public MainWindow()
         {
             polyPoints = new List<Point>();
@@ -27,7 +29,8 @@ namespace CG_Proj4
 
             InitializeComponent();
 
-            WriteableBitmap writeableBmp = BitmapFactory.New(835, 404);
+            writeableBmp = BitmapFactory.New(835, 718);
+
             mainImage.Source = writeableBmp;
         }
 
@@ -35,8 +38,8 @@ namespace CG_Proj4
         {
             if (mouseClicked)
             {
-                coord_X.Content = "X: " + e.GetPosition(mainCanvas).X;
-                coord_Y.Content = "Y: " + e.GetPosition(mainCanvas).Y;
+                coord_X.Content = "X: " + e.GetPosition(mainImage).X;
+                coord_Y.Content = "Y: " + e.GetPosition(mainImage).Y;
             }
         }
 
@@ -61,30 +64,6 @@ namespace CG_Proj4
 
         private void line_button_Click(object sender, RoutedEventArgs e)
         {
-
-            //int maxPolySize = 300;
-
-            //int startX = Convert.ToInt32(X1.Text);
-            //int startY = Convert.ToInt32(Y1.Text);
-
-            //int vCount = Convert.ToInt32(verticesCount.Text);
-            //if (vCount < 3)
-            //{
-            //    vCount = 3;
-            //    verticesCount.Text = "3";
-            //}
-
-            //Random rn = new Random();
-            //polyPoints = new List<Point>();
-            //for (int i = 0; i < vCount; i++)
-            //{
-
-            //    int tempX = rn.Next(startX - maxPolySize, startX + maxPolySize);
-            //    int tempY = rn.Next(startY - maxPolySize, startY + maxPolySize);
-
-            //    polyPoints.Add(new Point(tempX, tempY));
-            //}
-
             Polygon myPolygon = new Polygon();
             myPolygon.Stroke = System.Windows.Media.Brushes.Black;
             myPolygon.StrokeThickness = 2;
@@ -95,7 +74,17 @@ namespace CG_Proj4
 
             myPolygon.Points = myPointCollection;
 
-            mainCanvas.Children.Add(myPolygon);
+            int[] points = new int[polyPoints.Count*2];
+
+            for (int i = 0; i < polyPoints.Count * 2; i+=2)
+            {
+                points[i] = (int) polyPoints[i/2].X;
+                points[i + 1] = (int) polyPoints[i/2].Y;
+            }
+
+            int[] p = new int[] { 200, 150, 120, 340, 130, 330, 170, 185, 100, 50 };
+            writeableBmp.DrawPolyline(p, Color.FromRgb(0,0,0));
+
             polyPoints.Clear();
         }
 
@@ -115,35 +104,37 @@ namespace CG_Proj4
             int h = rnd.Next(100, 400);
             int w = rnd.Next(100, 400);
 
-            Shape shape = new System.Windows.Shapes.Rectangle
-            {
-                Stroke = Brushes.Black,
-                StrokeThickness = 2,
-                Height = h,
-                Width = w
-            };
+            writeableBmp.DrawRectangle((int)p.X, (int)p.Y, 120, 100, Colors.Black);
 
-            Shape el_shape = new System.Windows.Shapes.Ellipse
-            {
-                Stroke = Brushes.Black,
-                StrokeThickness = 2,
-                Height = h,
-                Width = w
-            };
+            //Shape shape = new System.Windows.Shapes.Rectangle
+            //{
+            //    Stroke = Brushes.Black,
+            //    StrokeThickness = 2,
+            //    Height = h,
+            //    Width = w
+            //};
 
-            List<Shape> shList = new List<Shape>();
-            shList.Add(shape);
-            shList.Add(el_shape);
+            //Shape el_shape = new System.Windows.Shapes.Ellipse
+            //{
+            //    Stroke = Brushes.Black,
+            //    StrokeThickness = 2,
+            //    Height = h,
+            //    Width = w
+            //};
 
-            Shape selShape = shList[rnd.Next(0, 2)];
-            mainCanvas.Children.Add(selShape);
-            Canvas.SetLeft(selShape, p.X - w);
-            Canvas.SetTop(selShape, p.Y - h);
+            //List<Shape> shList = new List<Shape>();
+            //shList.Add(shape);
+            //shList.Add(el_shape);
+
+            //Shape selShape = shList[rnd.Next(0, 2)];
+            //mainCanvas.Children.Add(selShape);
+            //Canvas.SetLeft(selShape, p.X - w);
+            //Canvas.SetTop(selShape, p.Y - h);
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            mainCanvas.Children.Clear();
+            writeableBmp.Clear();
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
@@ -157,17 +148,25 @@ namespace CG_Proj4
             switch (filling_type.Text)
             {
                 case "Scanline #1":
+                    Point stPt0 = new Point(cX, cY);
+                    Color target0 = Colors.White;
+                    Color repl0 = Colors.Blue;
 
+                    Algorithms.FloodFill4X(writeableBmp, stPt0, target0, repl0);
                     break;
                 case "Floodfill 4X":
                     Point stPt = new Point(cX, cY);
                     Color target = Colors.White;
-                    Color repl = Colors.Violet;
+                    Color repl = Colors.Blue;
 
-                    //Algorithms.FloodFill(mainCanvas, stPt, target, repl);
+                    Algorithms.FloodFill4X(writeableBmp, stPt, target, repl);
                     break;
                 case "Floodfill 8X":
+                    Point stPt2 = new Point(cX, cY);
+                    Color target2 = Colors.White;
+                    Color repl2 = Colors.Blue;
 
+                    Algorithms.FloodFill8X(writeableBmp, stPt2, target2, repl2);
                     break;
             }
         }
